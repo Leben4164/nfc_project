2a724da2399a6209968745e431431bded4fc29fb
Leben4164 <130670352+Leben4164@users.noreply.github.com>
2024-10-19 13:50:37 +0900

    살짝 수정

--- app/get/route.ts
+++ app/get/route.ts
@@ -2,11 +2,11 @@ import PocketBase from 'pocketbase';
 
 export async function GET(request : Request) {
     const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
-    const { propertyName } = await request.json(); // 요청 본문에서 속성 이름 가져오기
+    const res = await request.json(); // 요청 본문에서 속성 이름 가져오기
 
     try {
         const records = await pb.collection('students').getFullList(); // 모든 레코드 가져오기
-        const values = records.map(record => record[propertyName]); // 속성 이름에 해당하는 값들만 배열로 만들기
+        const values = records.map(record => record[res.propName]); // 속성 이름에 해당하는 값들만 배열로 만들기
         return new Response(JSON.stringify(values), { // 값 배열 반환
             headers: {
                 "Content-Type": "application/json",
