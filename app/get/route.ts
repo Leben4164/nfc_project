90eb7101d1cbd53c4318c76aa0d8ef474e800deb
idon190 <61539026+idon190@users.noreply.github.com>
2024-10-19 14:25:03 +0900

    Update route.ts

--- app/get/route.ts
+++ app/get/route.ts
@@ -5,9 +5,9 @@ export async function GET(request : Request) {
     const res = await request.json(); // 요청 본문에서 속성 이름 가져오기
 
     try {
-        const records = await pb.collection('students').getFullList(); // 모든 레코드 가져오기
-        const values = records.map(record => record[res.propName]); // 속성 이름에 해당하는 값들만 배열로 만들기
-        return new Response(JSON.stringify(values), { // 값 배열 반환
+        const records =  await pb.collection('students').getFirstListItem(`uid="${res.uid}"`)
+        //const values = records.map(record => record[res.propName]); // 속성 이름에 해당하는 값들만 배열로 만들기
+        return new Response(JSON.stringify(records), { // 값 배열 반환
             headers: {
                 "Content-Type": "application/json",
             },
