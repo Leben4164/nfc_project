2943ae97ae27c674c1221fd04fa864c968447a2b
idon190 <61539026+idon190@users.noreply.github.com>
2024-10-19 14:13:13 +0900

    Update route.ts

--- app/get/route.ts
+++ app/get/route.ts
@@ -6,9 +6,8 @@ export async function GET(request : Request) {
 
     try {
         const records = await pb.collection('students').getFullList(); // 모든 레코드 가져오기
-        //const values = records.map(record => record[res.propName]); // 속성 이름에 해당하는 값들만 배열로 만들기
-        //return new Response(JSON.stringify(values), { // 값 배열 반환
-        return new Response(JSON.stringify(records), {
+        const values = records.map(record => record[res.propName]); // 속성 이름에 해당하는 값들만 배열로 만들기
+        return new Response(JSON.stringify(values), { // 값 배열 반환
             headers: {
                 "Content-Type": "application/json",
             },
@@ -16,13 +15,11 @@ export async function GET(request : Request) {
         });
     } catch(error) {
         console.error(error);
-        return new Response(JSON.stringify({
-            "process" : "fail"
-        }), {
+        return new Response(JSON.stringify({ error: "Failed to fetch records" }), {
             headers: {
                 "Content-Type": "application/json",
             },
-            status: 201,
+            status: 500,
         });
     }
 }
