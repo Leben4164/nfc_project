f4c4fbb4078e34e5f1f66921f82389299cc86ef5
idon190 <61539026+idon190@users.noreply.github.com>
2024-10-19 13:28:04 +0900

    record 값 변경

--- get/route.ts
+++ get/route.ts
@@ -1,14 +1,16 @@
-import PocketBase from 'pocketbase';
+import PocketBase, { RecordModel } from 'pocketbase';
+import { useState, useEffect } from 'react';
 
 export async function GET(request : Request) {
     const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
     const res = request.json()
+    const [items, setItems] = useState<RecordModel[]>([]);
 
     try {
         const record = await pb.collection('students').getList(1, 50, {
             sort: '+studentId',
         });
-        setItems(record.items);
+        setItems(record.items)
         console.log(record.items)
     } catch(error) {
         console.error(error)
