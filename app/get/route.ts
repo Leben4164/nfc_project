e00da082f193550351616083894e864dea0a6ac9
Leben4164 <130670352+Leben4164@users.noreply.github.com>
2024-10-20 00:12:49 +0900

    본문에서 정보를 얻는 방식에서 search Param을 이용하는 방식으로 변경

--- app/get/route.ts
+++ app/get/route.ts
@@ -1,25 +1,29 @@
 import PocketBase from 'pocketbase';
 
-export async function GET(request : Request) {
-    const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
-    const res = await request.json(); // 요청 본문에서 속성 이름 가져오기
+export async function GET(request: Request) {
+  const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKETBASE_URL);
 
-    try {
-        const records = await pb.collection('students').getFullList(); // 모든 레코드 가져오기
-        const values = records.map(record => record[res.propName]); // 속성 이름에 해당하는 값들만 배열로 만들기
-        return new Response(JSON.stringify(values), { // 값 배열 반환
-            headers: {
-                "Content-Type": "application/json",
-            },
-            status: 200,
-        });
-    } catch(error) {
-        console.error(error);
-        return new Response(JSON.stringify({ error: "Failed to fetch records" }), {
-            headers: {
-                "Content-Type": "application/json",
-            },
-            status: 500,
-        });
-    }
+  // URL 쿼리 매개변수에서 propName 가져오기
+  const url = new URL(request.url);
+  const propName = url.searchParams.get("propName"); // 쿼리 매개변수에서 propName 가져오기
+
+  try {
+    const records = await pb.collection("students").getFullList(); // 모든 레코드 가져오기
+    const values = records.map((record) => record[propName!]); // 속성 이름에 해당하는 값들만 배열로 만들기
+    return new Response(JSON.stringify(values), {
+      // 값 배열 반환
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 200,
+    });
+  } catch (error) {
+    console.error(error);
+    return new Response(JSON.stringify({ error: "Failed to fetch records" }), {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      status: 500,
+    });
+  }
 }
